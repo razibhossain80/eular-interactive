e1f627219852a2f3a7e7dc79cd301ba3f17f15ae
razibhossain20 <razib.hossain@bs-23.net>
2022-04-11 13:45:32 +0600

    eular interactive project setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import Layout from '../components/Layout'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h2>This is home page</h2>
+      <p>Anim deserunt quis sint aliqua elit.</p>
+    </Layout>
+  )
 }
